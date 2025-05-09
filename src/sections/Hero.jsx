b44d5919899aab049296f088a971360006ab2787
Fatih Taner <ibrahimfatihtaner@gmail.com>
2025-05-10 01:12:24 +0300

    feat: started to design hero sections and elements

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,94 @@
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../contants";
+import Target from "../components/Target";
+import ReactLogo from "../components/ReactLogo";
+import Cube from "../components/Cube";
+import Rings from "../components/Rings";
+
+const Hero = () => {
+  //   const x = useControls("HackerRoom", {
+  //     positionX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionY: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionZ: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationY: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationZ: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     scale: {
+  //       value: 1,
+  //       min: 0.1,
+  //       max: 10,
+  //     },
+  //   });
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi , I am Fatih
+          <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Bulding Produts & Brands</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+            />
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
